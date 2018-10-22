78472645235a9eb7639d7ab446e0ad5c5fb81d4d
keblato <ks.blanco@uniandes.edu.co>
2018-10-22 12:24:12 -0500

    creacion de modulos

---
+++ src/app/billinginformation/billinginformation-list/billinginformation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billinginformation-list',
+  templateUrl: './billinginformation-list.component.html',
+  styleUrls: ['./billinginformation-list.component.css']
+})
+export class BillinginformationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/billinginformation/billinginformation.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BillinginformationListComponent } from './billinginformation-list/billinginformation-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BillinginformationListComponent]
+})
+export class BillinginformationModule { }
