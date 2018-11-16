2321096cf2ec075d4ddb86df99144ef1df422166
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-11-16 02:18:19 -0500

    mejoras

--- src/app/comentario/comentario.service.ts
+++ src/app/comentario/comentario.service.ts
@@ -3,11 +3,12 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { Comentario } from './comentario';
+import { LoginService } from '../UsuariosModule/services/login.service';
 
 @Injectable()
 export class ComentarioService {
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient,private auth: LoginService){}
 
   tipo: String;
 
@@ -17,6 +18,11 @@ export class ComentarioService {
     return this.http.get<Comentario[]>('http://localhost:8080/s3_tiendadiscos-api/api/'+this.tipo+'/1/comentarios/');
     }
   createComentarios(comentario): Observable<Comentario>{
-    return this.http.post<Comentario>('http://localhost:8080/s3_tiendadiscos-api/api/usuarios/1/comentarios/1',comentario)
+
+    
+
+
+
+    return this.http.post<Comentario>('http://localhost:8080/s3_tiendadiscos-api/api/usuarios/1/comentarios/'+this.auth.getUserObject().id,comentario)
   }
 }
