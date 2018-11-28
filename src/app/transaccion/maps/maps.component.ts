e79847d883e03b0126c51a2b972e35cb0473d123
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-11-28 18:50:58 -0500

    mapStyles

--- src/app/transaccion/maps/maps.component.ts
+++ src/app/transaccion/maps/maps.component.ts
@@ -33,6 +33,139 @@ interface Location {
 })
 export class MapsComponent implements OnInit {
   geocoder:any;
+  styles:any=[
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 13
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#144b53"
+        },
+        {
+          "lightness": 14
+        },
+        {
+          "weight": 1.4
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "stylers": [
+        {
+          "color": "#0c353e"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#0c4152"
+        },
+        {
+          "lightness": 5
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#ea064d"
+        },
+        {
+          "lightness": 15
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#ea064d"
+        },
+        {
+          "lightness": 25
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "color": "#146474"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "stylers": [
+        {
+          "color": "#021019"
+        }
+      ]
+    }
+  ]
+  
   public location:Location = {
     lat: 51.678418,
     lng: 7.809007,
